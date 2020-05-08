f05a676d6c99d19b4f1f60a648c0c22fcefc088c
Nagarjun <w3.nagarjuna@gmail.com>
2020-05-08 18:37:49 +0200

    jwt token updates

--- api/middlewares/check-is-admin.js
+++ api/middlewares/check-is-admin.js
@@ -3,7 +3,7 @@ var jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         var token = req.headers.authorization.split(" ")[1];
-        var userData = jwt.verify(token, process.env.JWT_KEY);
+        var userData = jwt.verify(token, process.env.JWT_ACCESS_KEY);
         if(userData.roles.indexOf('admin') !== -1){
             req.userData = userData;
             next();
